7117127eddc2af04d93208acfc735a5d2a757567
sumit <mazumdarsumit3@gmail.com>
2025-07-01 01:18:55 +0530

    feat(chore): added a functionality from clerk to create organizations

---
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { CreateOrganization } from "@clerk/nextjs";
+
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+
+export const NewButton = () => {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <div className="aspect-square">
+                    <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
+                        <Plus className="text-white"/>
+                    </button>
+                </div>
+            </DialogTrigger>
+            <DialogContent className="max-w-[480px] p-6 flex items-center justify-center">
+                <CreateOrganization/>
+            </DialogContent>
+        </Dialog>
+    );
+};
