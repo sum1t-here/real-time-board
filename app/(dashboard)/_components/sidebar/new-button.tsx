fc4e0a49b00fe37501bb3eba9233b09cdc0de883
sumit <mazumdarsumit3@gmail.com>
2025-07-02 00:27:53 +0530

    feat(style): revamp sidebar UI for improved layout and appearance

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -4,15 +4,22 @@ import { Plus } from "lucide-react";
 import { CreateOrganization } from "@clerk/nextjs";
 
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Hint } from "@/components/hint";
 
 export const NewButton = () => {
     return (
         <Dialog>
             <DialogTrigger asChild>
                 <div className="aspect-square">
-                    <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
-                        <Plus className="text-white"/>
-                    </button>
+                    <Hint 
+                        label="Create Organization"
+                        side="right"
+                        align="center"
+                    >
+                        <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
+                            <Plus className="text-white"/>
+                        </button>
+                    </Hint>
                 </div>
             </DialogTrigger>
             <DialogContent className="max-w-[480px] p-6 flex items-center justify-center">
